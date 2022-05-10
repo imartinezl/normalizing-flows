4ffa684b413aee6b4990b61205ac36b998f9570e
imartinezl <inigomlap@gmail.com>
2022-05-10 17:09:16 +0200

    Custom dataloader

--- base.py
+++ base.py
@@ -16,7 +16,7 @@ class NormalizingFlow(nn.Module):
     def forward(self, x, y=None):
         z, log_dz_dx_sum = x, torch.zeros_like(x)
         for transform in self.transforms:
-            z, log_dz_dx = transform(z, y)
+            z, log_dz_dx = transform.forward(z, y)
             log_dz_dx_sum += log_dz_dx
         return z, log_dz_dx_sum
 
@@ -34,18 +34,11 @@ def loss_function(model, target_distribution, z, log_dz_dx):
 def train(model, train_loader, optimizer, target_distribution):
     model.train()
     device = train_loader.dataset.device
-    for i, data in enumerate(train_loader):
-        # check if labeled dataset
-        if len(data) == 1:
-            x, y = data[0], None
-        else:
-            x, y = data
-            y = y.to(device)
-        x = x.view(x.shape[0], -1).to(device)
 
+    for i, (x, y) in enumerate(train_loader):
+        optimizer.zero_grad(set_to_none=True)
         z, log_dz_dx = model(x, y)
         loss = loss_function(model, target_distribution, z, log_dz_dx)
-        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
     return model
@@ -54,15 +47,7 @@ def eval_loss(model, data_loader, target_distribution):
     model.eval()
     device = data_loader.dataset.device
     total_loss = 0
-    for i, data in enumerate(data_loader):
-        # check if labeled dataset
-        if len(data) == 1:
-            x, y = data[0], None
-        else:
-            x, y = data
-            y = y.to(device)
-        x = x.view(x.shape[0], -1).to(device)
-
+    for i, (x, y) in enumerate(data_loader):
         z, log_dz_dx = model(x, y)
         loss = loss_function(model, target_distribution, z, log_dz_dx)
         total_loss += loss * x.size(0)
@@ -74,18 +59,27 @@ def train_and_eval(model, epochs, lr, train_loader, test_loader, target_distribu
     with tqdm(total=epochs) as pbar:
         for epoch in range(epochs):
             model = train(model, train_loader, optimizer, target_distribution)
+            # continue
             train_loss = eval_loss(model, train_loader, target_distribution)
             test_loss = eval_loss(model, test_loader, target_distribution)
             train_losses.append(train_loss)
             test_losses.append(test_loss)
+
+            # torch.save({
+            # 'epoch': epoch,
+            # 'model_state_dict': model.state_dict(),
+            # 'optimizer_state_dict': optimizer.state_dict(),
+            # 'train_loss': train_loss,
+            # 'test_loss': test_loss,
+            # }, PATH)
             pbar.set_description(f"Loss: train {train_loss:.4f}; test {test_loss:.4f}")
             pbar.update()
         pbar.close()
-    train_losses = np.array(train_losses)
-    test_losses = np.array(test_losses)
+    # train_losses = np.array(train_losses)
+    # test_losses = np.array(test_losses)
     return model, train_losses, test_losses
 
-def plot_loss(train_losses, test_losses):
+def plot_loss(train_losses, test_losses, ax=None): 
     plt.figure()
     plt.plot(train_losses, label='train_loss')
     plt.plot(test_losses, label='test_loss')
--- data.py
+++ data.py
@@ -36,6 +36,113 @@ class Dataset(torch.utils.data.Dataset):
         array /= array.std()
         return array
 
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, x, y=None):
+        super().__init__()
+        self.x = x
+        self.y = y
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        if self.y is None:
+            return [self.x[index]]
+        return self.x[index], self.y[index]
+
+class CustomSampler():
+    def __init__(self, dataset, batch_size, shuffle=False):
+        super().__init__()
+        data = dataset.tensors
+        if len(data) == 1:
+            x, y = data[0], None
+        else:
+            x, y = data
+        self.dataset = dataset
+        self.x = x
+        self.y = y
+        self.n = len(self.x)
+        self.batch_size = batch_size
+        self.n_batches = len(x) // batch_size
+        self.shuffle = shuffle
+
+    def __len__(self):
+        return len(self.x)
+
+    def __iter__(self):
+        if self.shuffle:
+            idx = torch.randperm(self.n)
+            self.x = self.x[idx]
+            if self.y is not None:
+                self.y = self.y[idx]
+        b = self.batch_size
+        for i in range(self.n_batches):
+            if self.y is None:
+                yield self.x[(i*b):((i+1)*b)], None
+            else:
+                yield self.x[(i*b):((i+1)*b)], self.y[(i*b):((i+1)*b)]
+
+
+
+class MultipleGPUSampler():
+    def __init__(self, dataset, batch_size, shuffle=False):
+        super().__init__()
+        data = dataset.tensors
+        if len(data) == 1:
+            x, y = data[0], None
+        else:
+            x, y = data
+
+        self.dataset = dataset
+        self.x = x
+        self.y = y
+        self.n = len(self.x)
+        # self.batch_size = batch_size
+        # self.n_batches = (len(x) // batch_size) + 1
+
+        # shuffle
+        if shuffle:
+            self.permutation()
+        
+        n_devices = torch.cuda.device_count()
+        devices = [torch.device("cuda:" + str(j)) for j in range(n_devices)]
+
+        # assign to device
+        xb = torch.split(self.x, batch_size)
+        if y is not None:
+            yb = torch.split(self.y, batch_size)
+        self.n_batches = len(xb)
+        print("Batch split", self.n_batches )
+
+        self.x = []
+        self.y = None if self.y is None else []
+        for i in range(self.n_batches):
+            dev = devices[i % n_devices]
+            self.x.append(xb[i].to(dev))
+            if y is not None:
+                self.y.append(yb[i].to(dev))
+        
+        print("Device assigned")
+
+    def permutation(self):
+        idx = torch.randperm(self.n)
+        self.x = self.x[idx]
+        if self.y is not None:
+            self.y = self.y[idx]
+
+    def __len__(self):
+        return len(self.x)
+
+    def __iter__(self):
+        for i in range(self.n_batches):
+            if self.y is None:
+                yield self.x[i], None
+            else:
+                yield self.x[i], self.y[i]
+
+          
+
+
 # %%
 
 def normalize_01(x, a, b):
@@ -71,9 +178,9 @@ def fetch_dataloaders(dataset_name, batch_size, device, toy_train_size=25000, to
         train_x = normalize_01(train_x, train_min, train_max)
         test_x = normalize_01(test_x, train_min, train_max)
 
-        # construct datasets
-        train_dataset = TensorDataset(torch.from_numpy(train_x))
-        test_dataset  = TensorDataset(torch.from_numpy(test_x))
+        # construct datasets/home/imartinez/.conda/envs/.venv/lib/python3.8/site-packages/torch/_utils.py
+        train_dataset = TensorDataset(torch.from_numpy(train_x).to(device))
+        test_dataset  = TensorDataset(torch.from_numpy(test_x).to(device))
 
         input_dims = dataset.n_dims
         label_size = None
@@ -96,8 +203,8 @@ def fetch_dataloaders(dataset_name, batch_size, device, toy_train_size=25000, to
         test_x = normalize_01(test_x, train_min, train_max)
 
         # construct datasets
-        train_dataset = TensorDataset(torch.from_numpy(train_x), torch.from_numpy(train_y))
-        test_dataset  = TensorDataset(torch.from_numpy(test_x), torch.from_numpy(test_y))
+        train_dataset = TensorDataset(torch.from_numpy(train_x).to(device), torch.from_numpy(train_y).to(device))
+        test_dataset  = TensorDataset(torch.from_numpy(test_x).to(device), torch.from_numpy(test_y).to(device))
 
         input_dims = dataset.n_dims
         label_size = 10
@@ -117,8 +224,8 @@ def fetch_dataloaders(dataset_name, batch_size, device, toy_train_size=25000, to
         test_x = normalize_01(test_x, train_min, train_max)
 
         # construct datasets
-        train_dataset = TensorDataset(torch.from_numpy(train_x))
-        test_dataset  = TensorDataset(torch.from_numpy(test_x))
+        train_dataset = TensorDataset(torch.from_numpy(train_x).to(device))
+        test_dataset  = TensorDataset(torch.from_numpy(test_x).to(device))
 
         input_dims = train_toy_dataset.input_size
         label_size = train_toy_dataset.label_size
@@ -139,9 +246,12 @@ def fetch_dataloaders(dataset_name, batch_size, device, toy_train_size=25000, to
         test_x = normalize_01(test_x, train_min, train_max)
 
         # construct datasets
-        train_dataset = TensorDataset(torch.from_numpy(train_x))
-        test_dataset  = TensorDataset(torch.from_numpy(test_x))
+        train_dataset = TensorDataset(torch.from_numpy(train_x).to(device))
+        test_dataset  = TensorDataset(torch.from_numpy(test_x).to(device))
 
+        # train_dataset = CustomDataset(torch.from_numpy(train_x))
+        # test_dataset  = CustomDataset(torch.from_numpy(test_x))
+              
         input_dims = 2
         label_size = None
         lam = None
@@ -167,11 +277,17 @@ def fetch_dataloaders(dataset_name, batch_size, device, toy_train_size=25000, to
     test_dataset.device = device
 
     # construct dataloaders
-    kwargs = {'num_workers': 1, 'pin_memory': True} if device.type is 'cuda' else {}
+    # kwargs = {'num_workers': 4, 'pin_memory': True} if device.type == 'cuda' else {}
+    # kwargs = {}
+    # train_loader = DataLoader(train_dataset, batch_size, shuffle=True, **kwargs)
+    # test_loader = DataLoader(test_dataset, batch_size, shuffle=False, **kwargs)
 
-    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, **kwargs)
-    test_loader = DataLoader(test_dataset, batch_size, shuffle=False, **kwargs)
+    train_loader = CustomSampler(train_dataset, batch_size, shuffle=True)
+    test_loader = CustomSampler(train_dataset, batch_size, shuffle=False)
     
+    # train_loader = MultipleGPUSampler(train_dataset, batch_size, shuffle=True)
+    # test_loader = MultipleGPUSampler(train_dataset, batch_size, shuffle=False)
+
     return train_loader, test_loader
 
 # %%
